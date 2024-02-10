02cb97f9af1047738a1ac6adbcf66254aefbaa8d
suhao <suhaoxb@gmail.com>
2024-02-10 21:23:02 +0700

    Wrapping axios into  request.js

---
+++ src/utils/request.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'http://cba.itlike.com/public/index.php?s=/api/',
+  timeout: 5000
+})
+
+// 请求拦截器
+request.interceptors.request.use(function (config) {
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+// 响应拦截器
+request.interceptors.response.use(function (response) {
+  return response.data
+}, function (error) {
+  return Promise.reject(error)
+})
+
+export default request
