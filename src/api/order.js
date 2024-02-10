e22d596cf7af87d0190fc851a326f9f1ab0d9bb4
suhao <suhaoxb@gmail.com>
2024-02-11 02:44:59 +0700

    Order Confirmation Page done

---
+++ src/api/order.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export const checkOrder = (mode, obj) => {
+  return request.get('/checkout/order', {
+    params: {
+      mode,
+      delivery: 10,
+      couponId: 0,
+      isUsePoints: 0,
+      ...obj
+    }
+  })
+}
+
+export const submitOrder = (mode, obj) => {
+  return request.post('/checkout/submit', {
+    mode,
+    delivery: 10,
+    couponId: 0,
+    isUsePoints: 0,
+    payType: 10,
+    ...obj
+  })
+}
+
+export const getMyOrderList = (dataType, page) => {
+  return request.get('/order/list', {
+    params: {
+      dataType,
+      page
+    }
+  })
+}
