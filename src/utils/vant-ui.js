da32741b7617bb95b6a30a21a1503ac8070d1f3b
suhao <suhaoxb@gmail.com>
2024-02-10 21:06:52 +0700

    Login page static layout done

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,7 @@
 // 按需导入
 import Vue from 'vue'
-import { Button, Tabbar, TabbarItem } from 'vant'
+import { Button, Tabbar, TabbarItem, NavBar } from 'vant'
 Vue.use(Button)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(NavBar)
